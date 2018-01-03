ef448bc9e700d944d3626274c91059a5fa5261f1
房明 <>
2018-01-03 10:39:34 +0800

    loadDemo

---
+++ FangMPodTestUtil/FMUtilTest.h
@@ -0,0 +1,13 @@
+//
+//  FMUtilTest.h
+//  podDemo
+//
+//  Created by 房明 on 2018/1/2.
+//  Copyright © 2018年 房明. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMUtilTest : NSObject
++ (void)logName;
+@end
---
+++ FangMPodTestUtil/FMUtilTest.m
@@ -0,0 +1,16 @@
+
+//
+//  FMUtilTest.m
+//  podDemo
+//
+//  Created by 房明 on 2018/1/2.
+//  Copyright © 2018年 房明. All rights reserved.
+//
+
+#import "FMUtilTest.h"
+
+@implementation FMUtilTest
++ (void)logName{
+    NSLog(@"fm");
+}
+@end
